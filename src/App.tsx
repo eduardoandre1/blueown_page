0fe892b49e57e095654abffe8628ebd8fc520d51
Eduardo André <eduardo_andre@id.uff.br>
2024-06-18 00:02:44 -0300

    fix :deploy format changing app.ccs to reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import Resetcss from "./utils/resetCSS"
 import MainPage from "./pages/mainPage"
-
+import "./App.css"
 
 
 function App() {
 
   return (
-    <>
+    <div className="App">
       <Resetcss />
       <MainPage />
-    </>
+    </div>
   )
 }
 
