149797a126cba5d215d6134a6d54527bf8a812e0
Eduardo André <eduardo_andre@id.uff.br>
2024-07-08 00:13:34 -0300

    fix deploy im map component

--- src/pages/blocks/map.tsx
+++ src/pages/blocks/map.tsx
@@ -9,9 +9,9 @@ export default function MapBlock(){
         <Mapblockstyle>
             <div className='titulo' >Siga nos em nossas redes Sociais</div>
             <div className='redeLinks' >
-                <a href="google.com"><img src={instagram} alt="" srcset="" />instagram</a>
-                <a href="google.com"><img src={linkedin} alt="" srcset="" />linkedin</a>
-                <a href="google.com"><img src={whatsapp} alt="" srcset="" />whatsaap</a>
+                <a href="google.com"><img src={instagram} alt="instagram"  />instagram</a>
+                <a href="google.com"><img src={linkedin} alt="linkedin"  />linkedin</a>
+                <a href="google.com"><img src={whatsapp} alt="whatsaap" />whatsaap</a>
             </div>
             <div className='titulo' >nossa localização</div>
             <a className="map" href="https://www.google.com/maps/place/R.+da+Concei%C3%A7%C3%A3o,+125+-+Centro,+Niter%C3%B3i+-+RJ,+24020-085/@-22.8940969,-43.1228631,17z/data=!3m1!4b1!4m6!3m5!1s0x9983c398751f5f:0xb30dc2799c8c5da7!8m2!3d-22.8940969!4d-43.1202882!16s%2Fg%2F11c1rtd04x?entry=ttu   ">
